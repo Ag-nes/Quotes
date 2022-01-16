000b05f7c0e21d44d56a9cd0bd6be47c58cab8bc
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-01-16 23:55:38 +0300

    Modified form

--- src/app/quote.spec.ts
+++ src/app/quote.spec.ts
@@ -1,7 +1,8 @@
 import { Quote } from './quote';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 describe('Quote', () => {
   it('should create an instance', () => {
-    expect(new Quote()).toBeTruthy();
+    expect(new QuoteDetailsComponent()).toBeTruthy();
   });
 });
